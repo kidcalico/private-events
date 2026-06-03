87d740d22b94e93026e0a0e61a32218aed47f6fe
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-06-03 15:03:28 -0700

    Create reference in Events to creator
    And added username column to to User model, along with authenication
    methods. Also generated devise User views, and a logout button to the
    top of the layout. This should probably be multiple commits...

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [ :username ]
+
+  validates :username, presence: true, uniqueness: true
+
+  before_save { self.username = username.downcase }
 
   has_many :created_events, dependent: :destroy, foreign_key: :creator_id, class_name: "Event"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_03_200637) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_03_214004) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "creator_id", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_03_200637) do
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
