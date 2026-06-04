9cab203059e47f2e249a07f1c8eacaba2e1d92cf
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-06-04 10:26:36 -0700

    Add #create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,21 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to events_path, notice: "Event successfully created."
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
 end
