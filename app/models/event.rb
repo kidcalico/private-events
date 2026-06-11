7f3518c65f1407edd70c1664a40220fdefb93b1e
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-06-11 12:50:00 -0700

    Create class methods Event#past and Event#upcoming

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: "User"
   has_many :invites, foreign_key: :attended_event_id
   has_many :attendees, through: :invites
+
+  def self.past
+    where("date < ?", Time.now)
+  end
+
+  def self.upcoming
+    where("date >= ?", Time.now)
+  end
 end
