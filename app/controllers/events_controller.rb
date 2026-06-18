8c2fc3db91b113eb0609b1fc2182e554b160a0f1
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-06-18 16:51:25 -0700

    Fix permissions for no user
    If no user is logged in, allow them to view any public events in the
    Event#index view. Also add buttons to sign up or log in from the main
    page when no user is signed in.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [ :index ]
   def index
     # @events = Event.all
     @past_events = Event.includes(:attendees, :host).past
